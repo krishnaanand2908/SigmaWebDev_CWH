a48ce5bc805d579056c9a658a84abf339bbbe927
krishnaanand2908 <krishnaanand290809@gmail.com>
2024-09-12 16:30:26 +0530

    after a long time

---
+++ JS-LEARNINGS/script.js
@@ -0,0 +1,8 @@
+var a = confirm("Enter this site?")
+
+if(a == true){
+    alert("Cross my heart and hope to die, welcome to my dark side")
+}
+else{
+    alert("You are forced to enter, you moron")
+}
